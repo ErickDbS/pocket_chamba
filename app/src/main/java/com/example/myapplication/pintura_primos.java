98b0628b8df232cc7d389f04b4595564e0186718
Carlos Garcia <111941326+CarlosGarcia20@users.noreply.github.com>
2023-11-26 01:43:28 -0700

    Se han agregado los layouts de los demas servicios que se ofrecen en la primera version

---
+++ app/src/main/java/com/example/myapplication/pintura_primos.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pintura_primos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pintura_primos);
+    }
+}
